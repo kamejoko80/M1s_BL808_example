cac8577e606f33c08c025bf50c30f5caae59c759
Henry Dang <phuongminh.dang@gmail.com>
2024-11-21 16:11:05 +0700

    Updated duktape_execute

--- c906_app/duktape_execute/duk_execute_cmd.c
+++ c906_app/duktape_execute/duk_execute_cmd.c
@@ -12,8 +12,15 @@
 /* Duktape */
 #include "duktape.h"
 
-extern duk_context *ctx;
 extern int duktape_execute_file(duk_context *ctx, const char *filename);
+
+static duk_ret_t native_print(duk_context *ctx) {
+    duk_push_string(ctx, " ");
+    duk_insert(ctx, 0);
+    duk_join(ctx, duk_get_top(ctx) - 1);
+    printf("%s\r\n", duk_safe_to_string(ctx, -1));
+    return 0;
+}
 
 void cmd_duk_execute(char *buf, int len, int argc, char **argv) {
 
@@ -21,14 +28,23 @@ void cmd_duk_execute(char *buf, int len, int argc, char **argv) {
     getopt_env_t getopt_env;
     utils_getopt_init(&getopt_env, 0);
 
+    duk_context *ctx = duk_create_heap_default();
+    if (!ctx) {
+        printf("Failed to create a Duktape heap.\r\n");
+        return;
+    }
+
+    // Register native print function call
+    duk_push_c_function(ctx, native_print, DUK_VARARGS);
+    duk_put_global_string(ctx, "print");
+
     for (; getopt_env.optind < argc; getopt_env.optind++) {
         //printf("extra arguments: %s\r\n", argv[getopt_env.optind]);
-        if(ctx != NULL) {
-            filename = argv[getopt_env.optind];
-            int result = duktape_execute_file(ctx, filename);
-            if (result != 0) {
-                printf("Error executing script: %s\r\n", filename);
-            }
+        filename = argv[getopt_env.optind];
+        int result = duktape_execute_file(ctx, filename);
+        if (result != 0) {
+            printf("Error executing script: %s\r\n", filename);
         }
     }
+    duk_destroy_heap(ctx);
 }
--- c906_app/duktape_execute/main.c
+++ c906_app/duktape_execute/main.c
@@ -27,27 +27,7 @@ const static struct cli_command cmds_user[] STATIC_CLI_CMD_ATTRIBUTE = {
     {"duk_execute", "Execute Duktaple JS file", cmd_duk_execute},
 };
 
-static duk_ret_t native_print(duk_context *ctx) {
-    duk_push_string(ctx, " ");
-    duk_insert(ctx, 0);
-    duk_join(ctx, duk_get_top(ctx) - 1);
-    printf("%s\r\n", duk_safe_to_string(ctx, -1));
-    return 0;
-}
-
 void main()
 {
     fatfs_register();
-
-    ctx = duk_create_heap_default();
-    if (!ctx) {
-        printf("Failed to create a Duktape heap.\r\n");
-        return;
-    }
-
-    // Register native print function call
-    duk_push_c_function(ctx, native_print, DUK_VARARGS);
-    duk_put_global_string(ctx, "print");
-
-    // duk_destroy_heap(ctx);
 }
