222383853988ffb855feaff1df2e5758b2b080d3
Henry Dang <phuongminh.dang@gmail.com>
2024-12-19 14:35:09 +0700

    Restructure jerryscript binding

--- c906_app/jerryscript_lvgl_binding/bouffalo.mk
+++ c906_app/jerryscript_lvgl_binding/bouffalo.mk
@@ -4,7 +4,9 @@
 # (Uses default behaviour of compiling all source files in directory, adding 'include' to include path.)
 
 COMPONENT_ADD_INCLUDEDIRS := .
-COMPONENT_ADD_INCLUDEDIRS += jerry-lvgl-binding
+COMPONENT_ADD_INCLUDEDIRS += jerry-lvgl-binding/common
+COMPONENT_ADD_INCLUDEDIRS += jerry-lvgl-binding/widgets
 
 COMPONENT_SRCDIRS := .
-COMPONENT_SRCDIRS += jerry-lvgl-binding
+COMPONENT_SRCDIRS += jerry-lvgl-binding/common
+COMPONENT_SRCDIRS += jerry-lvgl-binding/widgets
